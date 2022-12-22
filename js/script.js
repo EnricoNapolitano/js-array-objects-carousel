f1ba8336dc5bb13e27e85e89fb73bbff09f32e93
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-22 21:27:37 +0100

    hover on thumbnails

--- js/script.js
+++ js/script.js
@@ -1,26 +1,3 @@
-/*
-Dato un array di oggetti letterali con:
- - url dell’immagine
- - titolo
- - descrizione
-Creare un carosello come ispirandovi alla foto allegata. Se volete cambiare la grafica siete liberi di farlo.
-Milestone 0:
-Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
-Milestone 1:
-Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
-Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile assieme al suo titolo e testo.
-Milestone 2:
-Aggiungere il "ciclo infinito" del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
-BONUS 1:
-Aggiungere le thumbnails (sotto forma di miniatura) ed al click attivare l’immagine corrispondente.
-BONUS 2:
-Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
-BONUS 3:
-Aggiungere bottoni di start/stop  del meccanismo di autoplay.
-Buon lavoro e buon divertimento! :faccia_leggermente_sorridente:
-PS :avviso: Vi ricordo anche qui che l'orario dei ticket oggi è ridotto: 15:00 - 17:00
-*/
-
 //* FUNCTIONS
 const jumbotron = () => {
     const jumbo =`
@@ -41,6 +18,7 @@ const thumbnails = () => {
 };
 
 
+
 //* DOMS ELEMENT
 const jumboElm = document.querySelector('#main-carousel .jumbotron');
 const arrowUp = document.querySelector('#main-carousel .arrow.up');
@@ -51,6 +29,8 @@ const thumbsList = document.querySelector('#main-carousel ul');
 const gamesImage = data.map(image => image.image);
 const gamesTitle = data.map(title => title.title);
 const gamesText = data.map(text => text.text);
+
+
 
 let j = 0;
 jumbotron(); // this function generates the jumbotron
